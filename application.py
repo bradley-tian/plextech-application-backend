fixed normalization

application.py
@@ -32,29 +32,36 @@ admins = pymongo.collection.Collection(db, 'admins')
 trackers = pymongo.collection.Collection(db, 'trackers')
 errors = pymongo.collection.Collection(db, 'errors')
 
-@application.route('/add_applicant', methods = ['POST'])
-def addApplicant():
-    application = json.loads(request.get_data(as_text = True))
-    db.applicants.insert_one(application)
-    return jsonify(message = 'SUCCESS')
 
-@application.route('/get_applicant/<grader>', methods = ['GET'])
+@application.route('/add_applicant', methods=['POST'])
+def addApplicant():
+    application = json.loads(request.get_data(as_text=True))
+    db.applicants.insert_one(application)
+    return jsonify(message='SUCCESS')
+
+
+@application.route('/get_applicant/<grader>', methods=['GET'])
 def getApplicants(grader):
-    documents = list(db.applicants.find({"$and" : [{ 'assigned_to': grader }, { 'graded_by': {'$ne': grader}}]}))
+    documents = list(db.applicants.find(
+        {"$and": [{'assigned_to': grader}, {'graded_by': {'$ne': grader}}]}))
     return json.dumps(documents, default=str)
 
-@application.route('/add_review', methods = ['POST'])
-def addReview():
-    review = json.loads(request.get_data(as_text = True))
-    applicant = list(db.applicants.find({ "time_created" : review['applicantID'] }))[0]
-    applicant['graded_by'].append(review['grader'])
-    db.applicants.replace_one({ "time_created" : applicant['time_created'] }, applicant)
-    db.reviews.insert_one(review)
-    return jsonify(message = 'SUCCESS')
 
-@application.route('/check_grader', methods = ['POST'])
+@application.route('/add_review', methods=['POST'])
+def addReview():
+    review = json.loads(request.get_data(as_text=True))
+    applicant = list(db.applicants.find(
+        {"time_created": review['applicantID']}))[0]
+    applicant['graded_by'].append(review['grader'])
+    db.applicants.replace_one(
+        {"time_created": applicant['time_created']}, applicant)
+    db.reviews.insert_one(review)
+    return jsonify(message='SUCCESS')
+
+
+@application.route('/check_grader', methods=['POST'])
 def checkGrader():
-    grader = json.loads(request.get_data(as_text = True))
+    grader = json.loads(request.get_data(as_text=True))
     target = grader['email']
     check = list(db.graders.find({'email': target}))
     result = False
@@ -62,15 +69,17 @@ def checkGrader():
         result = True
     return json.dumps({"found": result}, default=str)
 
-@application.route('/add_admin', methods = ['POST'])
-def addAdmin():
-    admin = json.loads(request.get_data(as_text = True))
-    db.admins.insert_one(admin)
-    return jsonify(message = 'SUCCESS')
 
-@application.route('/check_admin', methods = ['POST'])
+@application.route('/add_admin', methods=['POST'])
+def addAdmin():
+    admin = json.loads(request.get_data(as_text=True))
+    db.admins.insert_one(admin)
+    return jsonify(message='SUCCESS')
+
+
+@application.route('/check_admin', methods=['POST'])
 def checkAdmin():
-    admin = json.loads(request.get_data(as_text = True))
+    admin = json.loads(request.get_data(as_text=True))
     target = admin['email']
     check = list(db.admins.find({'email': target}))
     result = False
@@ -78,25 +87,29 @@ def checkAdmin():
         result = True
     return json.dumps({"found": result}, default=str)
 
-@application.route('/get_graders', methods = ['GET'])
+
+@application.route('/get_graders', methods=['GET'])
 def getGraders():
     graders = list(db.graders.find())
     return json.dumps(graders, default=str)
 
-@application.route('/add_grader', methods = ['POST'])
-def addGrader():
-    grader = json.loads(request.get_data(as_text = True))
-    db.graders.insert_one(grader)
-    return jsonify(message = 'SUCCESS')
 
-@application.route('/remove_grader', methods = ['POST'])
+@application.route('/add_grader', methods=['POST'])
+def addGrader():
+    grader = json.loads(request.get_data(as_text=True))
+    db.graders.insert_one(grader)
+    return jsonify(message='SUCCESS')
+
+
+@application.route('/remove_grader', methods=['POST'])
 def removeGrader():
-    grader = json.loads(request.get_data(as_text = True))
+    grader = json.loads(request.get_data(as_text=True))
     target = grader['email']
     db.graders.delete_one({'email': target})
-    return jsonify(message = 'SUCCESS')
+    return jsonify(message='SUCCESS')
 
-@application.route('/analytics', methods = ['GET'])
+
+@application.route('/analytics', methods=['GET'])
 def getAnalytics():
     applicants = list(db.applicants.find())
     count = len(applicants)
@@ -137,7 +150,6 @@ def getAnalytics():
                 middle_eastern += 1
             elif race == "Native American or Other Pacific Islander":
                 pacific_islander += 1
-        
 
     result = {
         'count': count,
@@ -158,7 +170,8 @@ def getAnalytics():
 
     return json.dumps(result, default=str)
 
-@application.route('/assign_graders', methods = ['GET'])
+
+@application.route('/assign_graders', methods=['GET'])
 def assignGraders():
     graders = list(db.graders.find())
     applicants = list(db.applicants.find({'assigned_to': []}))
@@ -167,15 +180,15 @@ def assignGraders():
     current = int(tracker['current'])
     scope = len(graders)
 
-    if current >= scope: 
+    if current >= scope:
         current = current % scope
 
     for app in applicants:
         app['assigned_to'].append(graders[current]['email'])
         app['assigned_to'].append(graders[(current + 1) % scope]['email'])
-        db.applicants.replace_one({ "time_created": app['time_created'] }, app)
+        db.applicants.replace_one({"time_created": app['time_created']}, app)
         current = (current + 1) % scope
-    
+
     db.trackers.replace_one({'name': 'index'}, {'current': current})
 
     applicants = list(db.applicants.find())
@@ -183,13 +196,15 @@ def assignGraders():
     assignments = defaultdict(list)
 
     for app in applicants:
-        profile = str(app['first_name']) + " " + str(app['last_name']) + ", ID: " + str(app['time_created'])
+        profile = str(app['first_name']) + " " + \
+            str(app['last_name']) + ", ID: " + str(app['time_created'])
         for grader in app['assigned_to']:
             assignments[grader].append(profile)
 
     return json.dumps(assignments)
 
-@application.route('/export_results', methods = ['GET'])
+
+@application.route('/export_results', methods=['GET'])
 def exportResults():
     reviews = list(db.reviews.find())
     data = []
@@ -207,21 +222,22 @@ def exportResults():
         reader = csv.DictReader(f, fieldnames=reviews[0].keys())
         data = list(reader)
         f.close()
-    
+
     try:
         os.remove('results.csv')
     except:
         print("ERROR: CSV FILE NOT FOUND")
-        
+
     return json.dumps(data)
 
-@application.route('/export_applications', methods = ['GET'])
+
+@application.route('/export_applications', methods=['GET'])
 def exportApplications():
 
     applications = list(db.applicants.find())
-    for app in applications: 
+    for app in applications:
         app['resume'] = 'See Database'
-    data = [] 
+    data = []
 
     if len(applications) == 0:
         return json.dumps([])
@@ -236,15 +252,16 @@ def exportApplications():
         reader = csv.DictReader(f, fieldnames=applications[11].keys())
         data = list(reader)
         f.close()
-    
+
     try:
         os.remove('applications.csv')
     except:
         print("ERROR: CSV FILE NOT FOUND")
-        
+
     return json.dumps(data)
 
-@application.route('/flush_database', methods = ['GET'])
+
+@application.route('/flush_database', methods=['GET'])
 def flushDatabase():
     db.reviews.delete_many({})
 
@@ -252,26 +269,34 @@ def flushDatabase():
     for applicant in applicants:
         applicant['graded_by'] = []
         applicant['assigned_to'] = []
-        db.applicants.replace_one({ "time_created" : applicant['time_created'] }, applicant)
-    return jsonify(message = 'SUCCESS')
+        db.applicants.replace_one(
+            {"time_created": applicant['time_created']}, applicant)
+    return jsonify(message='SUCCESS')
 
-@application.route('/report_error', methods = ['POST'])
+
+@application.route('/report_error', methods=['POST'])
 def reportError():
-    report = json.loads(request.get_data(as_text = True))
+    report = json.loads(request.get_data(as_text=True))
     db.errors.insert_one(report)
-    return jsonify(message = 'SUCCESS')
+    return jsonify(message='SUCCESS')
 
-@application.route('/evaluate_results', methods = ['GET'])
+
+@application.route('/evaluate_results', methods=['GET'])
 def evaluateResults():
     reviews = list(db.reviews.find())
     judgments = defaultdict(lambda: defaultdict(list))
 
     for review in reviews:
-        judgments[review['grader']]['rating0'].append((int(review['rating0']), review['applicantID']))
-        judgments[review['grader']]['rating1'].append((int(review['rating1']), review['applicantID']))
-        judgments[review['grader']]['rating2'].append((int(review['rating2']), review['applicantID']))
-        judgments[review['grader']]['rating3'].append((int(review['rating3']), review['applicantID']))
-        judgments[review['grader']]['rating4'].append((int(review['rating4']), review['applicantID']))
+        judgments[review['grader']]['rating0'].append(
+            (int(review['rating0']), review['applicantID']))
+        judgments[review['grader']]['rating1'].append(
+            (int(review['rating1']), review['applicantID']))
+        judgments[review['grader']]['rating2'].append(
+            (int(review['rating2']), review['applicantID']))
+        judgments[review['grader']]['rating3'].append(
+            (int(review['rating3']), review['applicantID']))
+        judgments[review['grader']]['rating4'].append(
+            (int(review['rating4']), review['applicantID']))
 
     for grader in judgments:
         z_0 = stats.zscore([x[0] for x in judgments[grader]['rating0']])
@@ -281,22 +306,31 @@ def evaluateResults():
         z_4 = stats.zscore([x[0] for x in judgments[grader]['rating4']])
 
         for i in range(len(z_0)):
-            judgments[grader]['rating0'][i] = (z_0[i], judgments[grader]['rating0'][i][1])
-            judgments[grader]['rating1'][i] = (z_1[i], judgments[grader]['rating1'][i][1])
-            judgments[grader]['rating2'][i] = (z_2[i], judgments[grader]['rating2'][i][1])
-            judgments[grader]['rating3'][i] = (z_3[i], judgments[grader]['rating3'][i][1])
-            judgments[grader]['rating4'][i] = (z_4[i], judgments[grader]['rating4'][i][1])
+            judgments[grader]['rating0'][i] = (
+                z_0[i], judgments[grader]['rating0'][i][1])
+            judgments[grader]['rating1'][i] = (
+                z_1[i], judgments[grader]['rating1'][i][1])
+            judgments[grader]['rating2'][i] = (
+                z_2[i], judgments[grader]['rating2'][i][1])
+            judgments[grader]['rating3'][i] = (
+                z_3[i], judgments[grader]['rating3'][i][1])
+            judgments[grader]['rating4'][i] = (
+                z_4[i], judgments[grader]['rating4'][i][1])
 
     evaluations = defaultdict(lambda: defaultdict(list))
 
     for grader in judgments:
         for i in range(len(judgments[grader]['rating0'])):
-            evaluations[judgments[grader]['rating0'][i][1]]['rating0'].append(judgments[grader]['rating0'][i][0])
-            evaluations[judgments[grader]['rating1'][i][1]]['rating1'].append(judgments[grader]['rating1'][i][0])
-            evaluations[judgments[grader]['rating2'][i][1]]['rating2'].append(judgments[grader]['rating2'][i][0])
-            evaluations[judgments[grader]['rating3'][i][1]]['rating3'].append(judgments[grader]['rating3'][i][0])
-            evaluations[judgments[grader]['rating4'][i][1]]['rating4'].append(judgments[grader]['rating4'][i][0])
-
+            evaluations[judgments[grader]['rating0'][i][1]]['rating0'].append(
+                judgments[grader]['rating0'][i][0])
+            evaluations[judgments[grader]['rating1'][i][1]]['rating1'].append(
+                judgments[grader]['rating1'][i][0])
+            evaluations[judgments[grader]['rating2'][i][1]]['rating2'].append(
+                judgments[grader]['rating2'][i][0])
+            evaluations[judgments[grader]['rating3'][i][1]]['rating3'].append(
+                judgments[grader]['rating3'][i][0])
+            evaluations[judgments[grader]['rating4'][i][1]]['rating4'].append(
+                judgments[grader]['rating4'][i][0])
 
     for eval in evaluations:
         evaluations[eval]['rating0'] = np.mean(evaluations[eval]['rating0'])
@@ -309,8 +343,18 @@ def evaluateResults():
 
     for applicant in evaluations.keys():
         eval = {}
+
+        # Edit weighings here
+        w0, w1, w2, w3, w4 = 0.2, 0.2, 0.2, 0.2, 0.2
+
         eval['applicantID'] = applicant
         eval.update(evaluations[applicant])
+        eval['total'] = round(((eval['rating0'] * w0 +
+                         eval['rating1'] * w1 +
+                         eval['rating2'] * w2 +
+                         eval['rating3'] * w3 +
+                         eval['rating4'] * w4) * 100), 2)
+
         data.append(eval)
 
     export = []
@@ -328,16 +372,16 @@ def evaluateResults():
         reader = csv.DictReader(f, fieldnames=data[0].keys())
         export = list(reader)
         f.close()
-    
+
     try:
         os.remove('evaluations.csv')
     except:
         print("ERROR: CSV FILE NOT FOUND")
-        
+
     return json.dumps(export)
 
 
 if __name__ == '__main__':
     application.run(debug=True)
 
-#Rebooting Server Test
+# Rebooting Server Test